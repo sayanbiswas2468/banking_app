7375d3f637c264700af011e3e934ed578e545eef
Sayan Biswas <sayanbiswas2468@gmail.com>
2024-07-01 00:22:58 +0530

    implement the layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,40 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+
+    const loggedIn = { firstName: 'Sayan', lastName: 'Biswas', email: 'sayanbiswas2468@gmail.com' }
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || "Guest"}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1234.56}
+                    />
+
+                </header>
+
+
+                Recent transactions
+            </div>
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+            />
+        </section>
+    )
 }
 
 export default Home
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,16 @@
+'use client'
+import CountUp from 'react-countup'
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+    return (
+        <div className='w-full'>
+            <CountUp
+                decimals={2}
+                decimal=','
+                prefix='₹'
+                end={amount}
+            />
+        </div>
+    )
+}
+
+export default AnimatedCounter
