1bcbf0a01d0f0202ced5c3e0f3fc152eb00cdc50
Sayan Biswas <sayanbiswas2468@gmail.com>
2024-07-08 02:03:08 +0530

    Implement Plaid link and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,28 +1,30 @@
-'use client'
-import { Button } from "@/components/ui/button"
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+
+import { Button } from "@/components/ui/button";
 import {
     Form
-} from "@/components/ui/form"
-import { authFormSchema } from '@/lib/utils'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Loader2 } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useState } from 'react'
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import CustomInput from './CustomInput'
-import { useRouter } from "next/navigation"
-import {  signIn, signUp } from "@/lib/actions/user.actions"
-
-
+} from "@/components/ui/form";
+import { signIn, signUp } from '@/lib/actions/user.actions';
+import { authFormSchema } from '@/lib/utils';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import CustomInput from './CustomInput';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
-    const router = useRouter()
-    const [user, setUser] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
-    
-    const formSchema = authFormSchema(type)
+    const router = useRouter();
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const formSchema = authFormSchema(type);
+
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -34,37 +36,49 @@ const AuthForm = ({ type }: { type: string }) => {
 
     // 2. Define a submit handler.
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        setIsLoading(true)
+        setIsLoading(true);
 
         try {
-            //Sign up 
+            // Sign up with Appwrite & create plaid token
+
             if (type === 'sign-up') {
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+
+                const newUser = await signUp(userData);
+
                 setUser(newUser);
             }
+
             if (type === 'sign-in') {
                 const response = await signIn({
                     email: data.email,
                     password: data.password,
                 })
 
-                if (response) {
-                    router.push('/')
-                }
+                if (response) router.push('/')
             }
         } catch (error) {
             console.log(error);
-
         } finally {
             setIsLoading(false);
         }
     }
+
     return (
-        <section className='auth-form'>
+        <section className="auth-form">
             <header className='flex flex-col gap-5 md:gap-8'>
-                <Link href="/" className="cursor-pointer flex items-center gap-1 ">
+                <Link href="/" className="cursor-pointer flex items-center gap-1">
                     <Image
                         src="/icons/logo.svg"
                         width={34}
@@ -73,114 +87,81 @@ const AuthForm = ({ type }: { type: string }) => {
                     />
                     <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
                 </Link>
-                <div className='flex flex-col gap-1 md:gap-3'>
-                    <h1>
+
+                <div className="flex flex-col gap-1 md:gap-3">
+                    <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
                         {user
                             ? 'Link Account'
                             : type === 'sign-in'
                                 ? 'Sign In'
                                 : 'Sign Up'
-
                         }
-                        <p className='text-16 font-normal text-gray-600'>
+                        <p className="text-16 font-normal text-gray-600">
                             {user
-
                                 ? 'Link your account to get started'
-                                : 'Please enter your details'}
+                                : 'Please enter your details'
+                            }
                         </p>
                     </h1>
                 </div>
             </header>
             {user ? (
-                <div className='flex flex-col gap-4'>
-                    {/* Plade Link */}
+                <div className="flex flex-col gap-4">
+                    <PlaidLink user={user} variant="primary" />
                 </div>
             ) : (
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-
                             {type === 'sign-up' && (
                                 <>
-                                    <div className='flex gap-4'>
-                                        <CustomInput
-                                            control={form.control} name='firstName' label='First Name' placeholder='Enter your first name'
-                                        />
-                                        <CustomInput
-                                            control={form.control} name='lastName' label='Last Name' placeholder='Enter your last name'
-                                        />
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
+                                        <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your first name' />
                                     </div>
-                                    <CustomInput
-                                        control={form.control} name='address1' label='Address' placeholder='Enter your address'
-                                    />
-                                    <CustomInput
-                                        control={form.control} name='city' label='City' placeholder='Enter your city'
-                                    />
-                                    <div className='flex gap-4'>
-
-                                        <CustomInput
-                                            control={form.control} name='state' label='State' placeholder='Example: AP'
-                                        />
-                                        <CustomInput
-                                            control={form.control} name='postalCode' label='Postal Code' placeholder='Example: 500122'
-                                        />
+                                    <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
+                                    <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='state' label="State" placeholder='Example: NY' />
+                                        <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
                                     </div>
-                                    <div className='flex gap-4'>
-
-                                        <CustomInput
-                                            control={form.control} name='dateOfBirth' label='Date Of Birth' placeholder='DD-MM-YYYY'
-                                        />
-                                        <CustomInput
-                                            control={form.control} name='ssn' label='SSN' placeholder='Example: 1234'
-                                        />
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
+                                        <CustomInput control={form.control} name='ssn' label="SSN" placeholder='Example: 1234' />
                                     </div>
-
                                 </>
                             )}
 
-                            <CustomInput
-                                control={form.control} name='email' label='Email' placeholder='Enter your email'
-                            />
-                            <CustomInput
-                                control={form.control} name='password' label='Password' placeholder='Enter your password'
-                            />
-                            <div className='flex flex-col gap-4'>
+                            <CustomInput control={form.control} name='email' label="Email" placeholder='Enter your email' />
 
-                                <Button type="submit"
-                                    className='form-btn' disabled={isLoading}>
+                            <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
+
+                            <div className="flex flex-col gap-4">
+                                <Button type="submit" disabled={isLoading} className="form-btn">
                                     {isLoading ? (
                                         <>
-                                            <Loader2 size={20}
-                                                className='animate-spin' /> &nbsp;
+                                            <Loader2 size={20} className="animate-spin" /> &nbsp;
                                             Loading...
                                         </>
                                     ) : type === 'sign-in'
-                                        ? "Sign In" : 'Sign Up'
-
-                                    }
-
+                                        ? 'Sign In' : 'Sign Up'}
                                 </Button>
                             </div>
                         </form>
                     </Form>
 
-                    <footer className='flex justify-center gap-1'>
-                        <p className='text-14 font-normal text-gray-600'>
+                    <footer className="flex justify-center gap-1">
+                        <p className="text-14 font-normal text-gray-600">
                             {type === 'sign-in'
                                 ? "Don't have an account?"
-                                : "Already have an account"
-                            }
-
+                                : "Already have an account?"}
                         </p>
                         <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
                             {type === 'sign-in' ? 'Sign up' : 'Sign in'}
                         </Link>
-
                     </footer>
-
                 </>
-            )
-            }
+            )}
         </section>
     )
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,67 +1,326 @@
-'use server'
-import { ID } from "node-appwrite";
-import { createAdminClient, createSessionClient } from "../appwrite";
+"use server";
+
+import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
-import { parseStringify } from "../utils";
+import { ID, Query } from "node-appwrite";
+import {
+    CountryCode,
+    ProcessorTokenCreateRequest,
+    ProcessorTokenCreateRequestProcessorEnum,
+    Products,
+} from "plaid";
 
-export const signIn = async ({ email, password }: signInProps) => {
+import { plaidClient } from "@/lib/plaid";
+import {
+    parseStringify,
+    extractCustomerIdFromUrl,
+    encryptId,
+} from "@/lib/utils";
+
+import { createAdminClient, createSessionClient } from "../appwrite";
+
+import { addFundingSource, createDwollaCustomer } from "./dwolla.actions";
+
+const {
+    APPWRITE_DATABASE_ID: DATABASE_ID,
+    APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
+    APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
+} = process.env;
+
+export const signUp = async ({ password, ...userData }: SignUpParams) => {
+    let newUserAccount;
+
     try {
-        const { account } = await createAdminClient()
-        const response = await account.createEmailPasswordSession(email, password)
-        return parseStringify(response)
-    } catch (error) {
-        console.log('Error', error);
-
-    }
-}
-
-export const signUp = async (userData: SignUpParams) => {
-
-    const { email, password, firstName, lastName } = userData
-    try {
-        const { account } = await createAdminClient();
-
-        const newUserAccount = await account.create(
+        // create appwrite user
+        const { database, account } = await createAdminClient();
+        newUserAccount = await account.create(
             ID.unique(),
-            email,
+            userData.email,
             password,
-            `${firstName} ${lastName}`)
-        const session = await account.createEmailPasswordSession(email, password);
+            `${userData.firstName} ${userData.lastName}`
+        );
 
-        cookies().set("my-custom-session", session.secret, {
+        if (!newUserAccount) throw new Error("Error creating user");
+
+        // create dwolla customer
+        const dwollaCustomerUrl = await createDwollaCustomer({
+            ...userData,
+            type: "personal",
+        });
+
+        if (!dwollaCustomerUrl) throw new Error("Error creating dwolla customer");
+        const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+
+        const newUser = await database.createDocument(
+            DATABASE_ID!,
+            USER_COLLECTION_ID!,
+            ID.unique(),
+            {
+                ...userData,
+                userId: newUserAccount.$id,
+                dwollaCustomerUrl,
+                dwollaCustomerId,
+            }
+        );
+
+        const session = await account.createEmailPasswordSession(
+            userData.email,
+            password
+        );
+
+        cookies().set("appwrite-session", session.secret, {
             path: "/",
             httpOnly: true,
             sameSite: "strict",
             secure: true,
         });
-        return parseStringify(newUserAccount)
-    }
-    catch (error) {
-        console.log('Error', error);
 
-    }
-}
-
-
-
-export async function getLoggedInUser() {
-    try {
-        const { account } = await createSessionClient();
-        const user = await account.get();
-        return parseStringify(user);
+        return parseStringify(newUser);
     } catch (error) {
-        console.log(error);
+        console.error("Error", error);
+
+        // check if account has been created, if so, delete it
+        if (newUserAccount?.$id) {
+            const { user } = await createAdminClient();
+            await user.delete(newUserAccount?.$id);
+        }
+
         return null;
     }
-}
+};
 
-export const logoutAccount=async ()=>{
-    try{
-        const {account}=await createSessionClient()
-        cookies().delete('my-custom-session')
-        await account.deleteSession('current')
+export const signIn = async ({ email, password }: signInProps) => {
+    try {
+        const { account } = await createAdminClient();
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        const user = await getUserInfo({ userId: session.userId });
+
+        return parseStringify(user);
+    } catch (error) {
+        console.error("Error", error);
+        return null;
     }
-    catch(error){
-        return null
+};
+
+export const getLoggedInUser = async () => {
+    try {
+        const { account } = await createSessionClient();
+        const result = await account.get();
+
+        const user = await getUserInfo({ userId: result.$id });
+
+        return parseStringify(user);
+    } catch (error) {
+        console.error("Error", error);
+        return null;
     }
-}
+};
+
+// CREATE PLAID LINK TOKEN
+export const createLinkToken = async (user: User) => {
+    try {
+        const tokeParams = {
+            user: {
+                client_user_id: user.$id,
+            },
+            client_name: user.firstName + user.lastName,
+            products: ["auth"] as Products[],
+            language: "en",
+            country_codes: ["US"] as CountryCode[],
+        };
+
+        const response = await plaidClient.linkTokenCreate(tokeParams);
+
+        return parseStringify({ linkToken: response.data.link_token });
+    } catch (error) {
+        console.error(
+            "An error occurred while creating a new Horizon user:",
+            error
+        );
+    }
+};
+
+// EXCHANGE PLAID PUBLIC TOKEN
+// This function exchanges a public token for an access token and item ID
+export const exchangePublicToken = async ({
+    publicToken,
+    user,
+}: exchangePublicTokenProps) => {
+    try {
+        // Exchange public token for access token and item ID
+        const response = await plaidClient.itemPublicTokenExchange({
+            public_token: publicToken,
+        });
+
+        const accessToken = response.data.access_token;
+        const itemId = response.data.item_id;
+
+        // Get account information from Plaid using the access token
+        const accountsResponse = await plaidClient.accountsGet({
+            access_token: accessToken,
+        });
+
+        const accountData = accountsResponse.data.accounts[0];
+
+        // Create a processor token for Dwolla using the access token and account ID
+        const request: ProcessorTokenCreateRequest = {
+            access_token: accessToken,
+            account_id: accountData.account_id,
+            processor: "dwolla" as ProcessorTokenCreateRequestProcessorEnum,
+        };
+
+        const processorTokenResponse =
+            await plaidClient.processorTokenCreate(request);
+        const processorToken = processorTokenResponse.data.processor_token;
+
+        // Create a funding source URL for the account using the Dwolla customer ID, processor token, and bank name
+        const fundingSourceUrl = await addFundingSource({
+            dwollaCustomerId: user.dwollaCustomerId,
+            processorToken,
+            bankName: accountData.name,
+        });
+
+        // If the funding source URL is not created, throw an error
+        if (!fundingSourceUrl) throw Error;
+
+        // Create a bank account using the user ID, item ID, account ID, access token, funding source URL, and sharable ID
+        await createBankAccount({
+            userId: user.$id,
+            bankId: itemId,
+            accountId: accountData.account_id,
+            accessToken,
+            fundingSourceUrl,
+            sharableId: encryptId(accountData.account_id),
+        });
+
+        // Revalidate the path to reflect the changes
+        revalidatePath("/");
+
+        // Return a success message
+        return parseStringify({
+            publicTokenExchange: "complete",
+        });
+    } catch (error) {
+        // Log any errors that occur during the process
+        console.error("An error occurred while creating exchanging token:", error);
+    }
+};
+
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const user = await database.listDocuments(
+            DATABASE_ID!,
+            USER_COLLECTION_ID!,
+            [Query.equal("userId", [userId])]
+        );
+
+        if (user.total !== 1) return null;
+
+        return parseStringify(user.documents[0]);
+    } catch (error) {
+        console.error("Error", error);
+        return null;
+    }
+};
+
+export const createBankAccount = async ({
+    accessToken,
+    userId,
+    accountId,
+    bankId,
+    fundingSourceUrl,
+    sharableId,
+}: createBankAccountProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const bankAccount = await database.createDocument(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            ID.unique(),
+            {
+                accessToken,
+                userId,
+                accountId,
+                bankId,
+                fundingSourceUrl,
+                sharableId,
+            }
+        );
+
+        return parseStringify(bankAccount);
+    } catch (error) {
+        console.error("Error", error);
+        return null;
+    }
+};
+
+// get user bank accounts
+export const getBanks = async ({ userId }: getBanksProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const banks = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal("userId", [userId])]
+        );
+
+        return parseStringify(banks.documents);
+    } catch (error) {
+        console.error("Error", error);
+        return null;
+    }
+};
+
+// get specific bank from bank collection by document id
+export const getBank = async ({ documentId }: getBankProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const bank = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal("$id", [documentId])]
+        );
+
+        if (bank.total !== 1) return null;
+
+        return parseStringify(bank.documents[0]);
+    } catch (error) {
+        console.error("Error", error);
+        return null;
+    }
+};
+
+// get specific bank from bank collection by account id
+export const getBankByAccountId = async ({
+    accountId,
+}: getBankByAccountIdProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const bank = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal("accountId", [accountId])]
+        );
+
+        if (bank.total !== 1) return null;
+
+        return parseStringify(bank.documents[0]);
+    } catch (error) {
+        console.error("Error", error);
+        return null;
+    }
+};
